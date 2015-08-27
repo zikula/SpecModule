881226efc3c3374aafe5d20dc3853064e363a5b0
Craig Heydenburg <craigh@mac.com>
2015-08-26 20:06:54 -0400

    convert SpecModule to use @Admin annotation refs https://github.com/zikula/core/pull/2569

--- Controller/BarController.php
+++ Controller/BarController.php
@@ -7,12 +7,14 @@ use Zikula\Core\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route; // used in annotations - do not remove
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method; // used in annotations - do not remove
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template; // used in annotations - do not remove
+use Zikula\Core\Theme\Annotation\Admin;
 use Zikula\Core\Response\AdminResponse;
 
 class BarController extends AbstractController
 {
     /**
      * @Route("/admin/hello/{name}")
+     * @Admin
      * @param Request $request
      * @param string $name
      * @return array
@@ -20,6 +22,7 @@ class BarController extends AbstractController
     public function indexAction(Request $request, $name = 'no name')
     {
         $request->attributes->set('_legacy', true); // forces template to render inside old theme
-        return $this->render('ZikulaSpecModule:Bar:index.html.twig', array('name' => $name), new AdminResponse());
+//        return $this->render('ZikulaSpecModule:Bar:index.html.twig', array('name' => $name), new AdminResponse());
+        return $this->render('@ZikulaSpecModule/Bar/index.html.twig', array('name' => $name)); //, new AdminResponse());
     }
 }

dc50d5d55744d8980bb5c094e2588b925e222f52
Craig Heydenburg <craigh@mac.com>
2016-02-26 09:27:40 -0500

    Applied fixes from StyleCI

--- Controller/FooController.php
+++ Controller/FooController.php
@@ -5,7 +5,7 @@ namespace Zikula\SpecModule\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Zikula\Core\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route; // used in annotations - do not remove
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method; // used in annotations - do not remove
+// used in annotations - do not remove
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template; // used in annotations - do not remove
 
 class FooController extends AbstractController
@@ -19,11 +19,11 @@ class FooController extends AbstractController
      */
     public function indexAction(Request $request, $name = 'no name')
     {
-//        return $this->render('ZikulaSpecModule:Foo:index.html.twig', array('name' => $name));
+        //        return $this->render('ZikulaSpecModule:Foo:index.html.twig', array('name' => $name));
 //        return $this->render('@ZikulaSpecModule/Foo/index.html.twig', array('name' => $name));
         $perm = $this->hasPermission('ZikulaSpecModule::', '::', ACCESS_ADMIN, 1);
         $oldPerm = \SecurityUtil::checkPermission('ZikulaSpecModule::', '::', ACCESS_ADMIN, 1);
 
-        return ['name' => $name, 'perms' => ['old' => $oldPerm, 'new' =>$perm]];
+        return ['name' => $name, 'perms' => ['old' => $oldPerm, 'new' => $perm]];
     }
 }

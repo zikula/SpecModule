6352568f3f2453e706101ccf62de6ce4b2a8a330
Craig Heydenburg <craigh@mac.com>
2016-07-11 18:31:34 -0400

    remove old header

--- Block/Form/Type/FooBlockType.php
+++ Block/Form/Type/FooBlockType.php
@@ -9,18 +9,6 @@
  * file that was distributed with this source code.
  */
 
-/**
- * Copyright Zikula Foundation 2015 - Zikula Application Framework
- *
- * This work is contributed to the Zikula Foundation under one or more
- * Contributor Agreements and licensed to You under the following license:
- *
- * @license GNU/LGPLv3 (or at your option, any later version).
- *
- * Please see the NOTICE file distributed with this source code for further
- * information regarding copyright and licensing.
- */
-
 namespace Zikula\SpecModule\Block\Form\Type;
 
 use Symfony\Component\Form\AbstractType;

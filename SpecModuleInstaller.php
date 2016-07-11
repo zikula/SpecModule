729bb576d19a2be7547ac3ab3c25ce7f9e3eb6cf
Craig Heydenburg <craigh@mac.com>
2016-07-11 18:29:49 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- SpecModuleInstaller.php
+++ SpecModuleInstaller.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula Foundation - http://zikula.org/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Zikula\SpecModule;
 
 use Zikula\Core\AbstractExtensionInstaller;

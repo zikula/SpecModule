cb0d2b6d5536fcb4149018627fdb8d3c02a877ce
Christopher X. Candreva <chris@westnet.com>
2017-01-05 12:06:42 -0500

    Fix CI style errors.

--- Command/HelloCommand.php
+++ Command/HelloCommand.php
@@ -4,7 +4,6 @@ namespace Zikula\SpecModule\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -16,13 +15,13 @@ class HelloCommand extends ContainerAwareCommand
      * @var TranslatorInterface
      */
     protected $translator;
-    
+
     public function setContainer(ContainerInterface $container = null)
     {
         parent::setContainer($container);
         $this->translator = $container->get('translator.default');
-    }    
-    
+    }
+
     protected function configure()
     {
         $this
@@ -30,11 +29,11 @@ class HelloCommand extends ContainerAwareCommand
             ->setDescription('Print hello message.')
             ->setHelp('This command is just a test.');
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
         $io->text($this->translator->__('Hello, world.'));
     }
-    
+
 }
